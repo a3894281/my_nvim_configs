60c97516e97f48e35ca092f9df069e01e59a53bb
a3894281 <a3894281@gmail.com>
2024-11-13 20:16:24 +1300

    init settings.

---
+++ init.lua
@@ -0,0 +1,21 @@
+-- Initialize Packer
+vim.cmd [[packadd packer.nvim]]
+
+require('packer').startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- File explorer plugin (nvim-tree)
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = { 'kyazdani42/nvim-web-devicons' },
+    config = function()
+      require'nvim-tree'.setup {}
+    end
+  }
+
+  ----------------------------
+  -- Other useful plugins
+
+  
+end)
